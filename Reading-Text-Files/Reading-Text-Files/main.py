481988f298b342e48dc370cf3f5531d1710ecea9
acey954 <55606465+acey954@users.noreply.github.com>
2022-05-29 09:45:32 +0100

    Create main.py
    reading a text file and counting frequency of specific words in that file

---
+++ Reading-Text-Files/Reading-Text-Files/main.py
@@ -0,0 +1,31 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+from itertools import count
+
+
+def read_file_content(filename):
+    # [assignment] Add your code here 
+    with open("story.txt","r") as f:
+        file = f.read()
+    return file
+
+
+def count_words():
+    text = read_file_content("./story.txt")
+    # [assignment] Add your code here
+    words = text.split("")
+    dic = {}
+    for string in words:
+        if (string in count):
+            count[string] += 1
+        else:
+            count[string] - 1
+    for key, value in count.string():        
+            
+        return {"%s : %d"%(key, value)}
+
+
+print(count_words())
