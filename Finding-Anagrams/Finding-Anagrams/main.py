b0031c8bba3a5d902382b0462832cfcd47245410
acey954 <55606465+acey954@users.noreply.github.com>
2022-05-29 09:39:30 +0100

    Create main.py
    how to check for anagrams using the sorted function

---
+++ Finding-Anagrams/Finding-Anagrams/main.py
@@ -0,0 +1,31 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+from ast import Try
+from pickletools import string1
+
+
+
+def find_anagrams(word, anagram):
+    # [assignment] Add your code here
+    
+        if(sorted(s1)== sorted(s2)):
+
+
+            print (True)
+
+        else:
+
+            print (False)
+
+#map
+s1 ="hello"
+s2 ="check"
+s3 ="below"
+s4 ="elbow"
+find_anagrams(s1, s2)
+
+ 
